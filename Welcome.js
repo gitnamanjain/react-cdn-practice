f1b9356ccd70483d79de17e889db3c0d1f4da421
Naman Jain <replynamanjain@outlook.com>
2022-03-23 00:21:58 +0530

    v1.0

---
+++ Welcome.js
@@ -0,0 +1,9 @@
+class Welcome extends React.Component {
+    render() {
+        return (
+            <div class="Welcome">
+                <h2>Hey There</h2>
+            </div>
+        )
+    }
+}
